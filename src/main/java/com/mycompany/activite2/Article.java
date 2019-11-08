46fa20582448eb2537ca500dbc8ff1ec01137cc5
1995092 <1995092@LI-H022-0T11>
2019-11-08 18:25:48 -0500

    //classe Status

--- src/main/java/com/mycompany/activite2/Article.java
+++ src/main/java/com/mycompany/activite2/Article.java
@@ -21,7 +21,7 @@ import net.sf.json.JSONObject;
  * @author 1995092
  */
 public class Article {
-    
+
     private static Connection con = null;
     private static PreparedStatement stm = null;
     private static Statement stm1 = null;
@@ -29,9 +29,9 @@ public class Article {
     static Timestamp timestamp = new java.sql.Timestamp(System.currentTimeMillis());
     static JSONObject mainObject = new JSONObject();
 
-    public static JSONObject insertArticle(){
-       mainObject.clear();
-       try {
+    public static JSONObject insertArticle() {
+        mainObject.clear();
+        try {
             createConnection();
             String sql = "insert into  article "
                     + "values (?,?,?,?)";
@@ -41,12 +41,12 @@ public class Article {
             stm.setDouble(3, 10.1);
             stm.setInt(4, 20);
             int nombre = stm.executeUpdate();
-              if (nombre == 1) {
+            if (nombre == 1) {
                 mainObject = Status.getOkStatus(" article inserted");
             } else {
                 mainObject = Status.getErrorStatus("IN INSERT ARTICLE ");
             }
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(Article.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
@@ -57,31 +57,27 @@ public class Article {
 
         return mainObject;
     }
-    public static JSONObject updateArticle(int noartilce, double prixUnitaire) {
-       mainObject = new JSONObject();
-        try {
 
+    public static JSONObject updateArticle(int noarticle, String desc) {
+         mainObject.clear();
+        try {
             createConnection();
-            String sql = "update  article set prixunitaire=? where noartilce=?";
+            String sql = "update  article set DESCRIPTION=? where NOARTICLE=?";
             stm = con.prepareStatement(sql);
-            stm.setDouble(1, prixUnitaire);
-            stm.setInt(2, noartilce);
+            stm.setString(1,desc);
+            stm.setInt(2, noarticle);
             int nombre = stm.executeUpdate();
             if (nombre == 1) {
                 mainObject = Status.getOkStatus(" article updated");
             } else {
                 mainObject = Status.getErrorStatus("IN UPDATE ARTICLE ");
             }
-
         } catch (SQLException ex) {
             Logger.getLogger(Article.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
-
             closeConnection();
-
         }
         return mainObject;
-
     }
 
     public static JSONObject deleteArticle(int noarticle) {
@@ -109,18 +105,18 @@ public class Article {
     }
 
     public static JSONObject selectSimpleArticle(int noartilce) {
-        mainObject.clear();
+         mainObject.clear();
         try {
 
             createConnection();
-            String sql = "select * from article  where noartilce=?";
+            String sql = "select * from article  where noarticle=?";
             stm = con.prepareStatement(sql);
             stm.setInt(1, noartilce);
-
+            rs=stm.executeQuery();
             rs.next();
             if (rs != null) {
                 Status.getOkStatusSelect();
-                mainObject.accumulate("noartilce", rs.getInt("noartilce"));
+                mainObject.accumulate("noartilce", rs.getInt("noarticle"));
                 mainObject.accumulate("description", rs.getString("description"));
                 mainObject.accumulate("prix unitaire", rs.getString("prixunitaire"));
                 mainObject.accumulate("quantite en stock", rs.getString("quantiteenstock"));
@@ -143,7 +139,7 @@ public class Article {
         try {
 
             Class.forName("oracle.jdbc.OracleDriver");
-            con = DriverManager.getConnection("jdbc:oracle:thin:@144.217.163.57:1521:XE", "e19a10team2", "anypw");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@144.217.163.57:1521:XE", "vente", "anypw");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(Article.class.getName()).log(Level.SEVERE, null, ex);
         } catch (SQLException ex) {
--- src/test/java/com/mycompany/activite2/ArticleTest.java
+++ src/test/java/com/mycompany/activite2/ArticleTest.java
@@ -6,6 +6,7 @@
 package com.mycompany.activite2;
 
 import static com.mycompany.activite2.Article.mainObject;
+import static com.mycompany.activite2.Status.mainObject;
 import static com.mycompany.activite2.Status.timestamp;
 import net.sf.json.JSONObject;
 import org.junit.After;
@@ -18,79 +19,70 @@ import static org.junit.Assert.*;
  * @author 1995092
  */
 public class ArticleTest {
-    
+
     public ArticleTest() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-
-    /**
-     * Test of insertArticle method, of class Article.
-     */
-//    @Test
-//    public void testInsertArticle() {
-//        System.out.println("insertArticle");
-//        JSONObject expResult = null;
-//        JSONObject result = Article.insertArticle();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-
     /**
      * Test of updateArticle method, of class Article.
      */
     @Test
     public void testUpdateArticle() {
         System.out.println("updateArticle");
-        int noartilce = 20;
-        double prixUnitaire = 20;
-        JSONObject expResult = new JSONObject() ;
-        expResult.accumulate("Statut", "OK");
-        expResult.accumulate("Timestamp", timestamp.getTime());
-        expResult.accumulate("message", "" +"IN UPDATE ARTICLE ");
-         //result = new JSONObject() ;
-        JSONObject result = Article.updateArticle(20, 20.0);
-           System.out.println(expResult.getString("Statut"));
-           System.out.println(result.getString("Statut"));
-         //System.out.println(result);
-        assertEquals(expResult.getString("Statut"),result.getString("Statut"));
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
-    }
+        int noartilce = 80;
+        String desc = "test";
+        //JSONObject expResult = new JSONObject();
+        //expResult.accumulate("Statut", "OK");
+        JSONObject result = Article.updateArticle(noartilce, desc);
+        //System.out.println(result.getString("Status"));
+        assertEquals(result.getString("Status"), "OK");
 
+    }
     /**
      * Test of deleteArticle method, of class Article.
      */
-//    @Test
-//    public void testDeleteArticle() {
-//        System.out.println("deleteArticle");
-//        int noarticle = 0;
-//        JSONObject expResult = null;
-//        JSONObject result = Article.deleteArticle(noarticle);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+    @Test
+    public void testDeleteArticle() {
+        System.out.println("deleteArticle");
+        int noarticle = 100;
+        JSONObject expResult = new JSONObject();
+        expResult.accumulate("Statut", "OK");
+        JSONObject result = Article.deleteArticle(noarticle);
+        assertEquals(expResult.getString("Statut"), result.getString("Statut"));
+        // TODO review the generated test code and remove the default call to fail.
+
+    }
+
 
     /**
      * Test of selectSimpleArticle method, of class Article.
-//     */
-//    @Test
-//    public void testSelectSimpleArticle() {
-//        System.out.println("selectSimpleArticle");
-//        int noartilce = 0;
-//        JSONObject expResult = null;
-//        JSONObject result = Article.selectSimpleArticle(noartilce);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-    
+     */
+    @Test
+    public void testSelectSimpleArticle() {
+        System.out.println("selectSimpleArticle");
+        int noartilce = 100;
+        JSONObject result = Article.selectSimpleArticle(noartilce);
+        assertEquals(100, result.getInt("noartilce"));
+
+    }
+
+    /**
+     * Test of insertArticle method, of class Article.
+     */
+    @Test
+    public void testInsertArticle() {
+        System.out.println("insertArticle");
+        JSONObject expResult = new JSONObject();
+        expResult.accumulate("Statut", "OK");
+        JSONObject result = Article.insertArticle();
+        assertEquals(expResult.getString("Statut"), result.getString("Statut"));
+    }
+
 }
