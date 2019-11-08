3deb6573fdfe267e30f948c1dac56fef81e60022
1995150 <1995150@LI-H022-0T13>
2019-11-08 18:07:29 -0500

    //classe Commande e

--- src/test/java/com/mycompany/activite2/CommandeTest.java
+++ src/test/java/com/mycompany/activite2/CommandeTest.java
@@ -5,10 +5,10 @@
  */
 package com.mycompany.activite2;
 
+import java.util.Date;
 import net.sf.json.JSONObject;
-import net.sf.json.test.JSONAssert;
 import org.junit.After;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -33,53 +33,50 @@ public class CommandeTest {
      * Test of insertRecord method, of class Commande.
      */
     //  @Test
-    //  public void testInsertRecord() {
-//        System.out.println("insertRecord");
-//        JSONObject expResult = null;
-//        JSONObject result = Commande.insertRecord();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+     public void testInsertRecord() {
+
+    JSONObject result = new JSONObject();
+    result  = Commande.insertRecord();
+
+    assertEquals(result.getString("Statut"), "OK");
     //   }
     /**
      * Test of deleteRecord method, of class Commande.
      */
 //    @Test
-    // public void testDeleteRecord() {
-//        System.out.println("deleteRecord");
-//        int nocommande = 0;
-//        JSONObject expResult = null;
-//        JSONObject result = Commande.deleteRecord(nocommande);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-    //  }
-    /**
-     * Test of updateRecord method, of class Commande.
-     */
+//    public void testDeleteRecord() {
+//        int nocommande = 10;
+//        JSONObject result = new JSONObject();
+//        result = Commande.deleteRecord(nocommande);
+//        assertEquals(result.getString("Statut"), "OK");
+        //  }
+        /**
+         * Test of updateRecord method, of class Commande.
+         */
 //    @Test
 //    public void testUpdateRecord() {
-//         System.out.println("UpdateRecord");
+//        
 //        int nocommande =4;
 //        int noclient=30;
-//        JSONObject expResult = new JSONObject();
+// 
 //        JSONObject result = new JSONObject();
 //               result= Commande.updateRecord(nocommande, noclient);
-//        expResult.accumulate("Statut", "OK");
+
 //
-//        assertEquals(4, result.getInt("nocommande"));
-//       assertEquals(expResult, result.getString("OK"));
+//        assertEquals( result.getInt("Statut", "OK"), 4);
+
 //    }
-    /**
-     * Test of selectSimple method, of class Commande.
-     */
-    @Test
-    public void testSelectSimple() {       
-        int nocommande = 4;
-        JSONObject result = new JSONObject();
-        result = Commande.selectSimple(nocommande);
-        assertEquals(result.getInt("nocommande"), 4);
+        /**
+         * Test of selectSimple method, of class Commande.
+         */
+//    @Test
+//    public void testSelectSimple() {       
+//        int nocommande = 4;
+//        JSONObject result = new JSONObject();
+//        result = Commande.selectSimple(nocommande);
+//        assertEquals(result.getInt("nocommande"), 4);
+//
+//    }
 
     }
-
 }
