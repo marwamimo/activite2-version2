e395018e0422d4ad693efe767ca695fd1c0e67b6
1995092 <1995092@LI-H022-0T11>
2019-11-08 13:11:06 -0500

    //cclasse article

---
+++ src/main/java/com/mycompany/activite2/Article.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.activite2;
+
+/**
+ *
+ * @author 1995092
+ */
+public class Article {
+    
+}
