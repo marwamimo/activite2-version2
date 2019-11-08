5a87418481af89681658f0cc2760d85c8737dcd8
1995150 <1995150@LI-H022-0T13>
2019-11-08 16:05:45 -0500

    //classe Commande e

---
+++ src/test/java/com/mycompany/activite2/CommandeTest.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.activite2;
+
+import java.util.Date;
+import net.sf.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1995150
+ */
+public class CommandeTest {
+    
+    public CommandeTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of insertRecord method, of class Commande.
+     */
+    @Test
+    public void testInsertRecord() {
+//        System.out.println("insertRecord");
+//        JSONObject expResult = null;
+//        JSONObject result = Commande.insertRecord();
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of deleteRecord method, of class Commande.
+     */
+    @Test
+    public void testDeleteRecord() {
+//        System.out.println("deleteRecord");
+//        int nocommande = 0;
+//        JSONObject expResult = null;
+//        JSONObject result = Commande.deleteRecord(nocommande);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of updateRecord method, of class Commande.
+     */
+    @Test
+    public void testUpdateRecord() {
+//        System.out.println("updateRecord");
+//        int nocommande = 0;
+//        Date datecommande = null;
+//        JSONObject expResult = null;
+//        JSONObject result = Commande.updateRecord(nocommande, datecommande);
+//        assertEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of selectSimple method, of class Commande.
+     */
+    @Test
+    public void testSelectSimple() {
+        System.out.println("selectSimple");
+        JSONObject expResult = null;
+        System.out.println("blabala");
+//        JSONObject result = Commande.selectSimple();
+//        assertEquals(expResult, Commande.selectSimple());
+        // TODO review the generated test code and remove the default call to fail.
+       // fail("The test case is a prototype.");
+    }
+    
+}
